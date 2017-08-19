b649de9000155ceb9e424134ed3acc2d1b7064f2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-19 17:41:06 +0200

    query ISO3166-2 exactly

--- queries/ISO-3166-2.sql
+++ queries/ISO-3166-2.sql
@@ -1,18 +1,27 @@
 .mode column
 .headers on
+.width 7, 7, 7, 6, 20, 10
 
-.width 6, 20, 10
+--
+--  ISO 3166-2 identifies subdivisions of states (of ISO 3166-1).
+--  For Switzerland, those subdivisions are the cantons.
+--  We expect to find one record for each canton.
+--
 select
-  count(*),
+  nod_id,
+  way_id,
+  rel_id,
   key,
   val
 from
   tag
 where
-  lower(key) like '%iso%3166_2%' and
+  key = 'ISO3166-2' and
+--lower(key) like '%iso%3166_2%' and
   val like 'CH-%'
 group by
   key,
   val
 order by
-  count(*);
+  val
+;
