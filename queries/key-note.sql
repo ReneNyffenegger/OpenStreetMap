51529d63350e532b4b536d87c6e254b0b7064fa7
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-19 18:30:28 +0200

    + key-note.sql

---
+++ queries/key-note.sql
@@ -0,0 +1,36 @@
+.mode column
+.width 7 80
+select
+  count(*),
+  val
+from
+  tag
+where
+  key = 'note'
+group by
+  val
+order by
+  count(*);
+
+.mode column
+.width 7 7 7
+select
+  count(nod_id) cnt_nd,
+  count(way_id) cnt_wy,
+  count(rel_id) cnt_rl
+from
+  tag
+where
+  key = 'note';
+
+.mode column
+.width 10
+select
+  t1.nod_id
+from
+  tag t1    join
+  tag t2 on t1.nod_id = t2.nod_id
+where
+  t1.key = 'note' and
+  t2.key = 'note' and
+  t1.nod_id = t2.nod_id.;
