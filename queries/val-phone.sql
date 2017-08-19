e4506411b4807f7f9be3955c2fc314b1b4dc8ca8
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-19 18:36:43 +0200

    + val-phone.sql

---
+++ queries/val-phone.sql
@@ -0,0 +1,19 @@
+.mode column
+.width 7 20
+.header on
+
+select
+  count(*),
+  key
+from
+  tag
+where
+  val like '+41%'  or
+  val like '%076%' or
+  val like '%078%' or
+  val like '%079%'
+group by
+  key
+order by
+  count(*);
+
