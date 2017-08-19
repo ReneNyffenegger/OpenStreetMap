b21ce20561eb09f3a7be6f32f74de3c2eb9b8912
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-19 17:42:21 +0200

    count etc.

--- queries/websites.sql
+++ queries/websites.sql
@@ -14,3 +14,30 @@ from
   tag
 where
   key = 'website';
+
+.width 6 300
+select
+  count(*),
+  replace(replace(val, char(10), ' '), char(13), ' ')
+from
+  tag
+where
+  lower(val) like '%www.%' or
+  val like '%://%'
+group by
+  val
+order by
+  count(*);
+
+select
+  count(*),
+  key
+from
+  tag
+where
+  val like '%www.%' or
+  val like '%://%'
+group by
+  key
+order by
+  count(*);
