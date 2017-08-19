ca4ec690807ff61cced7dfd99a7edd44b1321ce7
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-19 18:30:34 +0200

    + key-note.sql

--- queries/key-note.sql
+++ queries/key-note.sql
@@ -33,4 +33,31 @@ from
 where
   t1.key = 'note' and
   t2.key = 'note' and
-  t1.nod_id = t2.nod_id.;
+  t1.rowid != t2.rowid
+;
+
+.mode column
+.width 10
+select
+  t1.way_id
+from
+  tag t1    join
+  tag t2 on t1.way_id = t2.way_id
+where
+  t1.key = 'note' and
+  t2.key = 'note' and
+  t1.rowid != t2.rowid
+;
+
+.mode column
+.width 10
+select
+  t1.rel_id
+from
+  tag t1    join
+  tag t2 on t1.rel_id = t2.rel_id
+where
+  t1.key = 'note' and
+  t2.key = 'note' and
+  t1.rowid != t2.rowid
+;
