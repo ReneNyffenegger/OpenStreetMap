4ff1590604ce27a2a066c97fd3f72ecae10e7ee6
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-21 15:58:19 +0200

    + scripts/pbf2sqlite.v2.py

---
+++ scripts/pbf2sqlite.v2.py
@@ -0,0 +1,286 @@
+#      Make sure pbf-parser is found:
+import sys
+sys.path.append('pbf-parser')
+
+import os
+import sqlite3
+import sys
+import time
+import OSMpbfParser
+
+cnt_node     =     0
+cnt_way      =     0
+cnt_relation =     0
+cnt_         = 10000
+
+def callback_node(node):
+
+    global cnt_node
+    cnt_node += 1
+    if cnt_node % cnt_ == 0:
+       print "Nodes: " + str(cnt_node)
+
+    cur.execute(
+      'insert into nod ' +
+      '(id, lat, lon) values (?, ?, ?)' , 
+      (node.NodeID, node.Lat, node.Lon))
+
+    for k in node.Tags.keys():
+
+        cur.execute(
+          'insert into tag ' +
+          '(nod_id, key, val) values (?, ?, ?)',
+          (node.NodeID, k, node.Tags[k]))
+
+
+def callback_way(way):
+
+    global cnt_way
+    cnt_way += 1
+    if cnt_way % cnt_ == 0:
+       print "Ways: " + str(cnt_way)
+
+    order_ = 0
+    for nd in way.Nds:
+
+        cur.execute(
+          'insert into nod_way '    +
+          '(way_id, nod_id, order_)'+
+          'values (?, ?, ?)',
+          (way.WayID, nd, order_))
+
+        order_ += 1
+
+
+    for k in way.Tags.keys():
+        cur.execute(
+         'insert into tag (way_id, key, val) values (?, ?, ?)',
+        (way.WayID, k, way.Tags[k]))
+
+
+def callback_relation(relation):
+
+    global cnt_relation
+    cnt_relation += 1
+    if cnt_relation % cnt_ == 0:
+       print "Relations: " + str(cnt_relation)
+
+    order_ = 0
+    for m in relation.Members:
+
+        if    m.type == 'node':
+
+              cur.execute("""
+                 insert into rel_mem (
+                   rel_of,
+                   order_,
+                   nod_id,
+                   rol
+                 ) values (?, ?, ?, ?) """,
+
+                 (relation.RelID, order_, m.ref, m.role))
+
+
+#v1              cur.execute("""
+#v1    
+#v1                insert into nod_rel (
+#v1                  nod_id,
+#v1                  rel_of,
+#v1                  rol
+#v1                )
+#v1                values (?, ?, ?) """,
+#v1                
+#v1                (m.ref, relation.RelID, m.role))
+
+        elif  m.type == 'way':
+
+              cur.execute("""
+                 insert into rel_mem (
+                   rel_of,
+                   order_,
+                   way_id,
+                   rol
+                 ) values (?, ?, ?, ?) """,
+
+                 (relation.RelID, order_, m.ref, m.role))
+
+#v.1          cur.execute("""
+#v.1          
+#v.1            insert into way_rel (
+#v.1              way_id,
+#v.1              rel_of,
+#v.1              rol
+#v.1            )
+#v.1            values (?, ?, ?) """,
+#v.1          
+#v.1          (m.ref, relation.RelID, m.role))
+
+        elif  m.type == 'relation': 
+
+              cur.execute("""
+                 insert into rel_mem (
+                   rel_of,
+                   order_,
+                   rel_id,
+                   rol
+                 ) values (?, ?, ?, ?) """,
+
+                 (relation.RelID, order_, m.ref, m.role))
+
+#v.1           cur.execute("""
+#v.1           
+#v.1              insert into rel_rel (
+#v.1                rel_id,
+#v.1                rel_of,
+#v.1                rol
+#v.1              )
+#v.1              values (?, ?, ?)""",
+#v.1           (m.ref, relation.RelID, m.role))
+
+        else: print "unexpected type: " + m.type
+
+        order_ += 1
+
+
+    for k in relation.Tags.keys():
+
+        cur.execute("""
+        
+            insert into tag (
+              rel_id,
+              key,
+              val
+            ) values (?, ?, ?)""",
+            (relation.RelID, k, relation.Tags[k]))
+
+def create_schema():
+    
+    cur.execute("""
+        create table nod (
+          id  integer primary key,
+          lat real not null,
+          lon real not null
+        )""")
+
+    cur.execute("""
+        create table nod_way (
+          way_id         integer not null,
+          nod_id         integer not null,
+          order_         integer not null
+        )""")
+
+    cur.execute("""
+        create table rel_mem (
+          rel_of         integer not null,
+          order_         integer not null,
+          way_id         integer,
+          nod_id         integer,
+          rel_id         integer,
+          rol            text
+       )""")
+
+
+#v.1    cur.execute("""
+#v.1        create table nod_rel (
+#v.1          nod_id         integer not null,
+#v.1          rel_of         integer null,
+#v.1          rol            text
+#v.1        )""")
+#v.1
+#v.1
+#v.1    cur.execute("""
+#v.1        create table way_rel (
+#v.1          way_id         integer not null,
+#v.1          rel_of         integer null,
+#v.1          rol            text
+#v.1        )""")
+#v.1
+#v.1    cur.execute("""
+#v.1        create table rel_rel (
+#v.1          rel_id         integer not null,
+#v.1          rel_of         integer null,
+#v.1          rol            text
+#v.1        )""")
+
+
+    cur.execute("""
+    
+        create table tag(
+          nod_id      integer null,
+          way_id      integer null,
+          rel_id      integer null,
+          key         text not null,
+          val         text not null
+        )""")
+
+
+def execute_sql(stmt):
+
+    t_ = time.time()
+
+    cur.execute(stmt)
+    
+    print "{:d} seconds for {:s}".format(
+          int(time.time() - t_), stmt)
+
+#   -----------------------------------------
+
+if len(sys.argv) != 3:
+   print "pbf2sqlite.py pbf-file sqlite-db-file"
+   sys.exit(0)
+
+# First argument is *.pbf file name
+pbf_filename = sys.argv[1]
+
+# second argument is *.db file name
+db_filename  = sys.argv[2]
+
+# delete db if exists
+if os.path.isfile(db_filename):
+   os.remove(db_filename)
+
+db  = sqlite3.connect(db_filename)
+db.text_factory = str
+
+# Makes inserts slower, so comment it:
+# db.execute('pragma foreign_keys=on')
+
+cur = db.cursor()
+
+create_schema()
+
+t_ = time.time()
+OSMpbfParser.go(
+  pbf_filename,
+  callback_node,
+  callback_way,
+  callback_relation)
+
+print "pbf file loaded, took {:d} seconds".format(int (time.time() -t_))
+
+
+t_ = time.time()
+db.commit()
+print "commited, took {:d} seconds".format(int (time.time() -t_))
+
+# execute_sql('create index nod_way_ix_way_id  on nod_ay (way_id)')
+# 
+# execute_sql('create index nod_way_ix_nod_id on nod_way (nod_id)')
+
+execute_sql('create index nod_way_ix_way_id on nod_way (way_id)'   )
+
+execute_sql('create index tag_ix_val        on tag     (     val)' )
+execute_sql('create index tag_ix_key_val    on tag     (key, val)' )
+
+execute_sql('create index tag_ix_nod_id     on tag     (nod_id)'   )
+execute_sql('create index tag_ix_way_id     on tag     (way_id)'   )
+execute_sql('create index tag_ix_rel_id     on tag     (rel_id)'   )
+
+# 
+# execute_sql('create index nod_rel_ix_nod_id on nod_rel(nod_id)' )
+# 
+# execute_sql('create index way_rel_ix_nod_id on way_rel(way_id)' )
+# 
+# execute_sql('create index rel_rel_ex_rel_id on rel_rel(rel_id)' )
+
+execute_sql('analyze' )
