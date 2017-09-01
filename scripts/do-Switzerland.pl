c9deeb092fb8625b6811dbcbf14c49f3a7a960a6
René Nyffenegger <mail@renenyffenegger.ch>
2017-09-01 21:27:40 +0200

    + municipalities_area_tables

--- scripts/do-Switzerland.pl
+++ scripts/do-Switzerland.pl
@@ -12,17 +12,17 @@ $dbh->{AutoCommit} = 0;
 my $osm_db = Geo::OSM::DBI::CH->new($dbh);
 
 municipalities();
+municipalities_area_tables();
 
-
-sub municipalities {
+sub municipalities { #_{
   $osm_db->create_table_municipalities_ch();
   $dbh->commit;
 
   my %municipalities = $osm_db->municipalities_ch;
 
   open (my $gotten, '>:utf8', 'gotten/municipalities_ch') or die;
-  for my $rel_id (sort { ( $municipalities{$a}{name  } cmp $municipalities{$b}{name  } ) or 
-                         ( $municipalities{$a}{bfs_no} <=> $municipalities{$b}{bfs_no} ) 
+  for my $rel_id (sort { ( $municipalities{$a}{name  } cmp $municipalities{$b}{name  } ) or
+                         ( $municipalities{$a}{bfs_no} <=> $municipalities{$b}{bfs_no} )
                        } keys %municipalities )
   {
     printf $gotten "%11d %-35s %4d %11.9f %11.9f %11.9f %11.9f\n",
@@ -33,7 +33,7 @@ sub municipalities {
       $municipalities{$rel_id}{lat_max},
       $municipalities{$rel_id}{lon_min},
       $municipalities{$rel_id}{lon_max};
-    
+
   }
   close $gotten;
 
@@ -42,5 +42,39 @@ sub municipalities {
   }
 
 
-}
+} #_}
+sub municipalities_area_tables { #_{
 
+  my %municipalities = $osm_db->municipalities_ch;
+
+  for my $rel_id (keys %municipalities) {
+
+    if ($municipalities{$rel_id}{name} eq 'Pfungen') {
+      print "Pfungen found: $rel_id\n";
+
+      my $name = $municipalities{$rel_id}{name};
+      $name =~ s/[(). ]/-/g;
+
+      $name = "$municipalities{$rel_id}{bfs_no}_$name";
+
+      print $name, "\n";
+#     next;
+
+
+      $dbh->{AutoCommit} = 1; # Why is this necessary?
+#     my $dbh = DBI->connect("dbi:SQLite:dbname=$db_test") or die "Could not create $db_test";
+      $dbh->do("attach database '../db/$name.db' as area");
+      $dbh->{AutoCommit} = 0; # Why is this necessary?
+
+      $osm_db->create_area_tables({schema_name_to=>'area', municipality_rel_id=>$rel_id});
+
+      $dbh->commit;
+      $dbh->{AutoCommit} = 1; # Why is this necessary?
+      $dbh->do("detach database area");
+      $dbh->{AutoCommit} = 0; # Why is this necessary?
+
+    }
+
+  }
+
+} #_}
