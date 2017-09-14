cf32925b2fe41d4ead053536606894f6dc0646fc
René Nyffenegger <mail@renenyffenegger.ch>
2017-09-14 21:15:59 +0200

    more todo.*

---
+++ queries/todo.rel-3887107.sql
@@ -0,0 +1,8 @@
+
+--
+-- The »Breiteackerstrasse« relation consists of ways as street and some buildings, too.
+--
+-- Others streets, such as »Wartbadstrasse« constists of ways only:
+--   25376863, 291534769 … 291534776
+--   
+--
--- queries/todo.sac.sql
+++ queries/todo.sac.sql
@@ -1,14 +1,41 @@
 .mode column
 .width 11 11 11 20 50
 
-select * from tag where (
-    upper(val) like 'SAC%'   or
-    upper(val) like '%SAC-%' or
-    upper(val) like '% SAC'  or
-    upper(val) like 'SAC %'  or
-    upper(val) like '%ALPINE HUT%'
-) and
-  upper(val) not like '%SACK%' and
-  upper(val) not like '%SACL%' and
-  upper(val) not like '%SACH%';
+--create table pot_sac as
+--select distinct
+--  nod_id,
+--  way_id,
+--  rel_id
+--from tag where (
+--    upper(val) like 'SAC%'   or
+--    upper(val) like '%SAC-%' or
+--    upper(val) like '% SAC'  or
+--    upper(val) like 'SAC %'  or
+--    upper(val) like '%ALPINE HUT%'
+--) and
+--  upper(val) not like '%SACK%' and
+--  upper(val) not like '%SACL%' and
+--  upper(val) not like '%SACH%';
 
+-- select
+--   count(*),
+--   key
+-- from
+--   pot_sac join
+--   tag     using (nod_id)
+-- group by
+--   key
+-- order by count(*);
+
+delete from pot_sac where nod_id in (select nod_id from tag where key = 'tourism' and val = 'alpine_hut');
+delete from pot_sac where way_id in (select way_id from tag where key = 'tourism' and val = 'alpine_hut');
+  
+.width 11 15 50
+select
+  nod_id,
+  key,
+  val
+from
+  pot_sac join
+  tag using (nod_id)
+;  
