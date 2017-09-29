778538ebb7fa2d4011fd6f41b30138d7c5bcae23
René Nyffenegger <mail@renenyffenegger.ch>
2017-09-29 18:22:58 +0200

    lat/lon

--- queries/alphuetten.sql
+++ queries/alphuetten.sql
@@ -3,7 +3,7 @@
 --
 
 .mode column
-.width 11 11 30 30 30 30 7 30
+.width 11 11 10 10 40 30 30 30 7 30
 
 with hut as (
   select distinct
@@ -18,23 +18,52 @@ with hut as (
   where
    ( hut.key = 'tourism'      and hut.val = 'alpine_hut'                         ) or
    ( hut.key = 'shelter_type' and hut.val = 'basic_hut'  and ele.val is not null )
+),
+cnd as (
+  select
+    hut.nod_id,
+    hut.way_id,
+    hut.elev,
+    nod.lat,
+    nod.lon
+  from
+    hut left join
+    nod on   hut.nod_id = nod.id 
+),
+cwy as (
+  select
+    cnd.nod_id,
+    cnd.way_id,
+    cnd.elev,
+    coalesce(cnd.lat, (max(nod.lat) + min(nod.lat)) / 2) lat,
+    coalesce(cnd.lon, (max(nod.lon) + min(nod.lon)) / 2) lon
+  from
+    cnd                                    left join
+    nod_way on cnd.way_id = nod_way.way_id left join
+    nod     on nod.id     = nod_way.nod_id
+  group by
+    cnd.nod_id,
+    cnd.way_id,
+    cnd.elev
 )
 select
-   hut.nod_id,
-   hut.way_id,
+   cwy.nod_id,
+   cwy.way_id,
+   cwy.lat,
+   cwy.lon,
    nam.val    name,
    nmd.val    name_de,
    nmf.val    name_fe,
    nme.val    name_en,
-   hut.elev   elev,
+   cwy.elev   elev,
    url.val    url
 from
-            hut /* indexed by tag_ix_key_val */                                                     left join
-   tag      url on  url.key    ='url'      and (hut.nod_id = url.nod_id or hut.way_id = url.way_id) left join
-   tag      nam on  nam.key    ='name'     and (hut.nod_id = nam.nod_id or hut.way_id = nam.way_id) left join
-   tag      nmd on  nmd.key    ='name:de'  and (hut.nod_id = nmd.nod_id or hut.way_id = nmd.way_id) left join
-   tag      nmf on  nmf.key    ='name:fr'  and (hut.nod_id = nmf.nod_id or hut.way_id = nmf.way_id) left join
-   tag      nme on  nmf.key    ='name:en'  and (hut.nod_id = nme.nod_id or hut.way_id = nme.way_id) 
+            cwy                                                                                     left join
+   tag      url on  url.key    ='url'      and (cwy.nod_id = url.nod_id or cwy.way_id = url.way_id) left join
+   tag      nam on  nam.key    ='name'     and (cwy.nod_id = nam.nod_id or cwy.way_id = nam.way_id) left join
+   tag      nmd on  nmd.key    ='name:de'  and (cwy.nod_id = nmd.nod_id or cwy.way_id = nmd.way_id) left join
+   tag      nmf on  nmf.key    ='name:fr'  and (cwy.nod_id = nmf.nod_id or cwy.way_id = nmf.way_id) left join
+   tag      nme on  nmf.key    ='name:en'  and (cwy.nod_id = nme.nod_id or cwy.way_id = nme.way_id) 
 order
-  by printf("%4d", hut.elev)
+  by printf("%4d", cwy.elev)
 --by coalesce(nmd.val, nam.val)
