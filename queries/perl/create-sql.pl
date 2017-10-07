c743db39523bb2a61a21d782316ae2922c0982dd
René Nyffenegger <mail@renenyffenegger.ch>
2017-10-07 16:26:34 +0200

    add lat/lon

--- queries/perl/create-sql.pl
+++ queries/perl/create-sql.pl
@@ -2,6 +2,10 @@
 use warnings;
 use strict;
 
+# use DBI;
+# my $dbh = DBI->connect('dbi:SQLite:dbname=../../db/ch.db') or die "Could not create the.db";
+
+
 
 my $key = 'amenity';
 my $val = 'parking';
@@ -22,12 +26,17 @@ my @cols = (
   { key => 'description'      , w =>    10},
 );
 
-my $dot_width = ".width 11 11 11";
-my $cols_stmt = 'ini.nod_id
-, ini.way_id
-, ini.rel_id';
+my $dot_width = ".width 11 11 11 11";
+# my $cols_stmt = 'ini.nod_id
+# , ini.way_id
+# , ini.rel_id';
+my $cols_stmt = 'wy.nod_id
+, wy.way_id
+, wy.lat
+, wy.lon';
 
-my $table_stmt = ' tag ini';
+# my $table_stmt = ' tag ini';
+my $table_stmt = ' wy';
 
 my $no_alias = 0;
 for my $col (@cols) { #_{
@@ -38,7 +47,9 @@ for my $col (@cols) { #_{
  (my $col_name  = $key) =~ s/:/_/g;
 
   $cols_stmt  .= "\n, $alias.val $col_name";
-  $table_stmt .= " left join\n   tag $alias on $alias.key ='$key' and (ini.nod_id = $alias.nod_id or ini.way_id = $alias.way_id or ini.rel_id = $alias.rel_id)";
+# $table_stmt .= " left join\n   tag $alias on $alias.key ='$key' and (ini.nod_id = $alias.nod_id or ini.way_id = $alias.way_id or ini.rel_id = $alias.rel_id)";
+# $table_stmt .= " left join\n   tag $alias on $alias.key ='$key' and (wy.nod_id = $alias.nod_id or wy.way_id = $alias.way_id or wy.rel_id = $alias.rel_id)";
+  $table_stmt .= " left join\n   tag $alias on $alias.key ='$key' and (wy.nod_id = $alias.nod_id  or wy.way_id = $alias.way_id /* or wy.rel_id = $alias.rel_id */)";
 
   $dot_width .= " $col->{w}";
 
@@ -46,17 +57,59 @@ for my $col (@cols) { #_{
 
 } #_}
 
-my $sql_stmt = "select
+my $sql_stmt = "
+with ini as (
+  select
+    nod_id,
+    way_id
+  from
+    tag
+  where
+    key = '$key' and
+    val = '$val'
+),
+nd as (
+  select
+    ini.nod_id,
+    ini.way_id,
+    nod.lat,
+    nod.lon
+  from
+    ini left join
+    nod on ini.nod_id = nod.id
+),
+wy as (
+  select
+    nd.nod_id,
+    nd.way_id,
+    coalesce(nd.lat, (max(nod.lat) + min(nod.lat)) / 2) lat,
+    coalesce(nd.lon, (max(nod.lon) + min(nod.lon)) / 2) lon
+  from
+    nd                                    left join
+    nod_way on nd.way_id = nod_way.way_id left join
+    nod     on nod.id    = nod_way.nod_id
+  group by
+    nd.nod_id,
+    nd.way_id
+)
+select
   $cols_stmt
 from
   $table_stmt
-where
-  ini.key = '$key' and
-  ini.val = '$val'
-";
+-- where
+--   ini.key = '$key' and
+--   ini.val = '$val'
+;";
+
+
+# my $sth = $dbh->prepare($sql_stmt);
+# $sth->execute;
+# while (my $r = $sth->fetchrow_hashref) {
+#}
 
 
 print ".mode column\n";
 print ".header on\n";
+print ".timer on\n";
 print "$dot_width\n";
 print $sql_stmt;
