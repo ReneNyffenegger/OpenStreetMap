05c9a1f8724feb4c62ebdf351a67e870249c3748
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-10-22 21:16:09 +0200

    + key-val_all.sql

---
+++ queries/key-val_all.sql
@@ -0,0 +1,22 @@
+.mode   column
+.width  11 11 11 20 50
+
+select
+  gt.nod_id,
+  gt.way_id,
+  gt.rel_id,
+  gt.key,
+  gt.val
+from
+  tag tg                              join
+  tag gt on tg.nod_id = gt.nod_id or
+            tg.way_id = gt.way_id or
+            tg.rel_id = gt.rel_id
+where
+  tg.key = 'amenity' and
+--tg.val = 'parking'
+  tg.val = 'fuel'
+order by
+  tg.nod_id,
+  tg.way_id,
+  tg.rel_id
