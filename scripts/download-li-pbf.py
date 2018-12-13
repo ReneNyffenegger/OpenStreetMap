5d0a94d3f3e73a9f1297907487d8649299c005c6
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-13 12:26:29 +0100

    python 3

--- scripts/download-li-pbf.py
+++ scripts/download-li-pbf.py
@@ -1,6 +1,7 @@
-import urllib
+#!/usr/bin/python3
+import urllib.request   
 import time
 
 t_0 = time.time()
-urllib.urlretrieve  ('http://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf'  , '../pbf/li.pbf'  )
-print "Download took {:d} seconds".format(int(time.time() - t_0))
+urllib.request.urlretrieve  ('http://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf'  , '../pbf/li.pbf'  )
+print("Download took {:d} seconds".format(int(time.time() - t_0)))
