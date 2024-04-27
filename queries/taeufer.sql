3ffb7e63bddd17d62dafc216b8607d462c355ced
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-04-27 21:41:26 +0200

    do not show some clutter

--- queries/taeufer.sql
+++ queries/taeufer.sql
@@ -1,5 +1,13 @@
 select * from tag where
+(
   lower(key) like '%täufer%'  or
   lower(val) like '%täufer%'  or
   lower(key) like '%baptist%' or
-  lower(val) like '%baptist%';
+  lower(val) like '%baptist%'
+)
+and not (key = 'name' and val = 'Avenue de-Baptista')
+and not (key = 'addr:street' and val like ('Chemin Jean-Baptiste%'))
+and not (key = 'addr:street' and val like ('Rue Baptiste%'))
+and not (key = 'addr:street' and val = 'Avenue de-Baptista')
+and not (key = 'artist_name' and val = 'Baptist Hörbst')
+;
